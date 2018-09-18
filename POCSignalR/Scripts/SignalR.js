02e11ed19ac6ff97b65d0ce2441a704f304550a7
Codruta Sipos <sipos_codruta@yahoo.com>
2018-09-18 13:52:46 +0300

    add signalR logging

--- POCSignalR/Scripts/SignalR.js
+++ POCSignalR/Scripts/SignalR.js
@@ -1,19 +1,14 @@
 ﻿(function () {
-	$("#click-me").on("click", function () {
-		chat.server.getServerDateTime()
-			.done(function (data) {
-				writeToPage(data)
-			})
-			.fail(function (error) {
-				writeToPage("Error getting the server dateTime: " + error);
-			});
-	});
 	//var myHub = $.connection.myHub;//INFO: camelCase is a must. On .cs the class name is "MyHub", in js it needs to be "myHub"
 	var chat = $.connection.ChatHub;// INFO: if HubName attributes are used, the name needs to be exactly the same. In this case "ChatHub". And it's mandatory to use the hubName now, the class name won't work anymore
+	
 	$.connection.hub.start()
 		.done(function () {
+			$.connection.hub.logging = true;//INFO: enable logging. On Chrome, "Verbose"/"All" needs to be selected in order to see the logs on Console
 			//console.log('it worked');
 			writeToPage('It worked');
+			$.connection.hub.log("Log test: Connected");
+			console.log('Console Log test');
 			chat.server.announceToEverybody("tada!!");
 			
 			//myHub.server.getServerDateTime2()
@@ -38,6 +33,16 @@
 	var writeToPage = function (message) {
 		$("#welcome-messages").append(message + "<br/>");
 	}
+
+	$("#click-me").on("click", function () {
+		chat.server.getServerDateTime()
+			.done(function (data) {
+				writeToPage(data)
+			})
+			.fail(function (error) {
+				writeToPage("Error getting the server dateTime: " + error);
+			});
+	});
 })()
 
 
